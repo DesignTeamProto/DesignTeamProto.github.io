e1a5ddb8bf6531880c9cadec321b9c942a657833
DesignTeamProto <43267835+DesignTeamProto@users.noreply.github.com>
2018-10-25 09:23:42 +0200

    Updated build

---
+++ precache-manifest.4bf87e7313c22b0b1cf40b278f906981.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "76c52d50f1d25f2ff739f13aaa7a1d92",
+    "url": "/components/static/media/logo.76c52d50.svg"
+  },
+  {
+    "revision": "701db38576110deb18f3",
+    "url": "/components/static/js/runtime~main.701db385.js"
+  },
+  {
+    "revision": "cc921aad6d787a847e87",
+    "url": "/components/static/js/main.cc921aad.chunk.js"
+  },
+  {
+    "revision": "5ab763f2281c7847c4d4",
+    "url": "/components/static/js/1.5ab763f2.chunk.js"
+  },
+  {
+    "revision": "cc921aad6d787a847e87",
+    "url": "/components/static/css/main.173483b2.chunk.css"
+  },
+  {
+    "revision": "63d098649718fd22c415840d77615851",
+    "url": "/components/index.html"
+  }
+];
